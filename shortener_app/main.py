6c66fb532130bd2199c3099dd9938f3034e05018
Igor <nikitinigorra@gmail.com>
2024-02-16 19:59:10 +0200

    removed extra line breaks in main.py

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -1,12 +1,10 @@
 import validators
 from fastapi import FastAPI, HTTPException
-
 from . import schemas
 
 app = FastAPI()
 
 def raise_bad_request(message):
-
     raise HTTPException(status_code=400, detail=message)
 
 @app.get("/")
@@ -14,11 +12,7 @@ def read_root():
     return "Welcome to the URL shortener API :)"
 
 @app.post("/url")
-
 def create_url(url: schemas.URLBase):
-
     if not validators.url(url.target_url):
-
         raise_bad_request(message="Your provided URL is not valid")
-
     return f"TODO: Create database entry for: {url.target_url}"
