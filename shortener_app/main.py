30a2421545e0fbf40d2cdcc0efcb0317acedc6e6
Igor <nikitinigorra@gmail.com>
2024-02-21 17:27:28 +0200

    updated main code

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -15,24 +15,23 @@ def get_db():
     finally:
         db.close()
 
-# ...
-
 @app.post("/url", response_model=schemas.URLInfo)
 def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
     if not validators.url(url.target_url):
         raise_bad_request(message="Your provided URL is not valid")
-
-    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    key = "".join(secrets.choice(chars) for _ in range(5))
-    secret_key = "".join(secrets.choice(chars) for _ in range(8))
-    db_url = models.URL(
-        target_url=url.target_url, key=key, secret_key=secret_key
-    )
-    db.add(db_url)
-    db.commit()
-    db.refresh(db_url)
     db_url = crud.create_db_url(db=db, url=url)
-    db_url.url = key
-    db_url.admin_url = secret_key
+    db_url.url = db_url.key
+    db_url.admin_url = db_url.secret_key
 
     return db_url
+
+@app.get("/{url_key}")
+def forward_to_target_url(
+        url_key: str,
+        request: Request,
+        db: Session = Depends(get_db)
+    ):
+    if db_url := crud.get_db_url_by_key(db=db, url_key=url_key):
+        return RedirectResponse(db_url.target_url)
+    else:
+        raise_not_found(request)
